1b92f0ec179ae4764d75f0c89e61aec5fa88de17
Yunus <skysoft.founder@gmail.com>
2023-12-28 18:37:11 +0300

    Initial Commit

---
+++ ToDoListDeneme2/ToDoListDeneme2App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListDeneme2App.swift
+//  ToDoListDeneme2
+//
+//  Created by Yunus Can on 28/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListDeneme2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
